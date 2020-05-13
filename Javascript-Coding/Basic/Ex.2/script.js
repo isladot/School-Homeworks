5bf60401f2c044a03fa510ec1d5bacdefd784163
Luca Biagetti <notciabaku@gmail.com>
2020-05-13 12:30:33 +0200

    Basic 2 Upload
    Argomenti:
    - alert
    - parseFloat
    - Math.round

---
+++ Javascript-Coding/Basic/Ex.2/script.js
@@ -0,0 +1,18 @@
+var temp, tempconv;
+
+function inputTemp(){
+    temp = parseFloat(prompt("Prego, inserire la temperatura (°F):", "100"));
+}
+function convertTemp(){
+    var multipler = 10;
+    tempconv = (temp-32)/1.8;
+    tempconv = Math.round(tempconv*multipler)/multipler;
+}
+function dispTemp(){
+    alert("Temperatura convertita in °C = " + tempconv);
+}
+function Temp(){
+    inputTemp();
+    convertTemp();
+    dispTemp();
+}
