2c0e5e97e9ffe98bae92a6e3e756d4019fc0f1eb
Luca Biagetti <notciabaku@gmail.com>
2020-09-18 22:12:59 +0200

    Upload random 1

---
+++ Cpp-Coding/Random/random1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    const int MAX_DIM = 10;
+    int A[10], B[10], bCont = 0;
+    for(int i=0; i<MAX_DIM;i++){
+        A[i] = (rand() % 50);
+        if((A[i]>=5 && A[i]<=7) || (A[i]>=12 && A[i]<=15)){
+            B[bCont] = A[i];
+            bCont++;
+        }
+    }
+    cout<<"Questi sono i numeri presenti nell'array A:" <<endl;
+    for(int i=0;i<MAX_DIM;i++){
+        cout<<A[i] <<" ";
+    }
+    cout<<endl<<"Questi sono i numeri che rispettavano i criteri per essere inseriti nell'array B:" <<endl;
+    for(int i=0;i<bCont;i++){
+        cout<<B[i] <<" ";
+    }
+}
