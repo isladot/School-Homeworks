71f35e661845e060810e337979548381546080df
Luca Biagetti <notciabaku@gmail.com>
2020-12-03 12:47:51 +0100

    [JS] Survey v2.

--- Javascript/4BI/Survey/script.js
+++ Javascript/4BI/Survey/script.js
@@ -12,22 +12,25 @@ function calculate(){
     for(let value of document.getElementById('terminated').elements){
         if(value.checked && value.id === 'q4a3') good_answers++;
     }
+    for(let value of document.getElementById('ready').elements){
+        if(value.checked && value.id === 'q5a2') good_answers++;
+    }
     let message;
     switch(good_answers){
-        case 0:
-            message = "Giudizio pessimo.";
-            break;
-        case 1:
+        case 2:
             message = "Giudizio insufficiente.";
             break;
-        case 2:
+        case 3:
             message = "Giudizio sufficiente.";
             break;
-        case 3:
+        case 4:
             message = "Giudizio distinto.";
             break;
-        case 4:
+        case 5:
             message = "Giudizio ottimo.";
+            break;
+        default:
+            message = "Giudizio pessimo.";
             break;
     }
     alert(message);
