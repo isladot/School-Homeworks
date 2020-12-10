58fdc0a8b5997c3d8ae1a2d6d838b7d941008a60
Luca Biagetti <notciabaku@gmail.com>
2020-12-10 22:18:09 +0100

    [C++] upload files3 with some cool stuffs in it.

---
+++ Cpp/4BI/Files/Files3/files3.cpp
@@ -0,0 +1,65 @@
+/*
+Dato un file in input contenente dei record con Cognome, Nome ed eta', caricare questi dati in una tabella.
+- 1: Ordinare la tabella per cognome crescente.
+- 2: Scrivere su file i record aventi eta'>=18 anni
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//N. of max people.
+const int MAX_PEOPLE = 10;
+//N. of current people.
+int current_people = 0;
+
+//Person record.
+struct person {
+    string surname;
+    string name;
+    int age;
+};
+
+//People array of person records.
+person people[MAX_PEOPLE];
+
+void sort_table();
+void print_overage();
+
+int main(){
+    //Files.
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    //Read file.
+    for(int i=0; i<MAX_PEOPLE; i++){
+        cin>>people[i].surname >>people[i].name >>people[i].age;
+        if(people[i].surname == "") break;
+        current_people++;
+    }
+
+    sort_table();
+    print_overage();
+
+    return 0;
+}
+
+void sort_table(){
+    person tmp;
+
+    for(int i=0; i<current_people; i++){
+        for(int j=i+1; j<current_people; j++){
+            if(people[i].surname > people[j].surname){
+                tmp = people[i];
+                people[i] = people[j];
+                people[j] = tmp;
+            }
+        }
+    }
+}
+
+void print_overage(){
+    for(int i=0; i<current_people; i++){
+        if(people[i].age >= 18) cout<<people[i].surname <<" " <<people[i].name <<endl;
+    }
+}
