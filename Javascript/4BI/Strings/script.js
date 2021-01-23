9733642e44ecf40d3e487fba00722112aac5b237
Luca Biagetti <notciabaku@gmail.com>
2021-01-23 23:07:00 +0100

    [JS] upload strings functions.

---
+++ Javascript/4BI/Strings/script.js
@@ -0,0 +1,34 @@
+function validateForm() {
+    //Getting form input.
+    const inputName = document.getElementById('inputName');
+    const inputSurname = document.getElementById('inputSurname');
+    const inputEmail = document.getElementById('inputEmail');
+
+    //Validating name.
+    if (inputName.value == '' || inputName.value == null) {
+        alert('Si prega di inserire un nome valido.');
+        inputName.classList.remove('good');
+        inputName.classList.add('error');
+    } else {
+        inputName.classList.remove('error');
+        inputName.classList.add('good');
+    }
+    //Validating surname.
+    if (inputSurname.value == '' || inputSurname.value == null) {
+        alert('Si prega di inserire un cognome valido.');
+        inputName.classList.remove('good');
+        inputSurname.classList.add('error');
+    } else {
+        inputSurname.classList.remove('error');
+        inputSurname.classList.add('good');
+    }
+    //Validating email address.
+    if (inputEmail.value == '' || inputEmail.value == null || !inputEmail.value.includes('@' && '.')) {
+        alert('Si prega di inserire un indirizzo email valido.');
+        inputName.classList.remove('good');
+        inputEmail.classList.add('error');
+    } else {
+        inputEmail.classList.remove('error');
+        inputEmail.classList.add('good');
+    }
+}
