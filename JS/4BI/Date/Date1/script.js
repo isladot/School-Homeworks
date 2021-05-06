4b2105129727c8fb815c323012862a0d5f611df5
Luca Biagetti <notciabaku@gmail.com>
2021-05-06 12:24:57 +0200

    [JS] upload date1: checkBefore, checkAfter.

---
+++ JS/4BI/Date/Date1/script.js
@@ -0,0 +1,18 @@
+function validateForm(){
+  let nameValue = document.getElementById('name').value.toLowerCase();
+  let surnameValue = document.getElementById('surname').value.toLowerCase();
+  let startDateValue = new Date(document.getElementById('start-date').value);
+  let endDateValue = new Date(document.getElementById('end-date').value);
+  
+  if(nameValue == '' || surnameValue == ''){
+    alert('Inserire nome e cognome validi.');
+    return;
+  } else {
+    if(isNaN(startDateValue) || startDateValue < new Date()){
+      alert('Data di arrivo non valida.');
+    }
+    if(isNaN(endDateValue) || endDateValue < new Date() || endDateValue < startDateValue){
+      alert('Data di partenza non valida.');
+    }
+  }
+}
